451c99ada6dc436eb54a3f2c969d2223957236d1
StevenKowalzik <steven@kowalzik.de>
2018-01-24 15:57:06 +0100

    No Grinds fix and tricks-out variations

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@ const grinds = ['nosegrind', '50-50', '5-0', 'Feeble', 'Smith', 'Crooked']
 const slides = ['noseslide', 'tailslide', 'boardslide', 'lipslide', 'bluntslide', 'nosebluntslide']
 const stance = ['regular', 'regular', 'regular', 'switch', 'fakie', 'nollie']
 const direction = ['frontside', 'backside']
-const tricks = ['kickflip', 'heelflip', 'shoveit', 'fsshoveit', '', '']
-const rotation = ['fs180', 'bs180', '','','',''] /* what about popout, pop over? */
+const tricks = ['kickflip', 'heelflip', 'shoveit', 'fsshoveit', '', '', '', '']
+const rotation = ['fs180', 'bs180', 'Pop out','Pop over','','']
 const trickDisplay = document.querySelector('#trick')
 const button = document.querySelector('button')
 const sound = document.querySelector('audio')
@@ -11,9 +11,9 @@ button.addEventListener('click', generateTrick)
 
 function generateTrick() {
   let railtrick;
-  if (Math.random < 0.5) { railtrick = grinds }
+  if (Math.random() < 0.5) { railtrick = grinds }
   else { railtrick = slides }
-  let trick = `Do a ${choose(stance)} ${choose(tricks)} ${choose(direction)} ${choose(railtrick)}`
+  let trick = `Do a ${choose(stance)} ${choose(tricks)} ${choose(direction)} ${choose(railtrick)} ${choose(rotation)}`
   trickDisplay.innerHTML = trick
   if (trick.search('heelflip') != -1) {
     sound.play()
