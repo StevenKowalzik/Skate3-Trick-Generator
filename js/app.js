11dd5a2a709d2fd3a862780dd0f00b9177aa9a7f
StevenKowalzik <steven@kowalzik.de>
2018-02-05 19:45:21 +0100

    regular out and html title

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-const grinds = ['nosegrind', '50-50', '5-0', 'Feeble', 'Smith', 'Crooked']
-const slides = ['noseslide', 'tailslide', 'boardslide', 'lipslide', 'bluntslide', 'nosebluntslide']
-const stance = ['regular', 'regular', 'regular', 'switch', 'fakie', 'nollie']
-const direction = ['frontside', 'backside']
-const tricks = ['kickflip', 'heelflip', 'shoveit', 'fsshoveit', '', '', '', '']
-const rotation = ['fs180', 'bs180', 'Pop out','Pop over','','']
-const trickDisplay = document.querySelector('#trick')
-const button = document.querySelector('button')
-const sound = document.querySelector('audio')
-button.addEventListener('click', generateTrick)
+const grinds = ['nosegrind', '50-50', '5-0', 'Feeble', 'Smith', 'Crooked'];
+const slides = ['noseslide', 'tailslide', 'boardslide', 'lipslide', 'bluntslide', 'nosebluntslide'];
+const stance = ['', '', '', 'switch', 'fakie', 'nollie'];
+const direction = ['frontside', 'backside'];
+const tricks = ['kickflip', 'heelflip', 'shoveit', 'fsshoveit', '', '', '', ''];
+const rotation = ['fs180', 'bs180', 'Pop out','Pop over','',''];
+const trickDisplay = document.querySelector('#trick');
+const button = document.querySelector('button');
+const sound = document.querySelector('audio');
+button.addEventListener('click', generateTrick);
 
 function generateTrick() {
   let railtrick;
-  if (Math.random() < 0.5) { railtrick = grinds }
-  else { railtrick = slides }
-  let trick = `Do a ${choose(stance)} ${choose(tricks)} ${choose(direction)} ${choose(railtrick)} ${choose(rotation)}`
-  trickDisplay.innerHTML = trick
+  if ( Math.random() < 0.5 ) { railtrick = grinds; }
+  else { railtrick = slides; }
+  let trick = `Do a ${choose(stance)} ${choose(tricks)} ${choose(direction)} ${choose(railtrick)} ${choose(rotation)}`;
+  trickDisplay.innerHTML = trick;
   if (trick.search('heelflip') != -1) {
-    sound.play()
+    sound.play();
   }
 }
 
-const choose = tricks => tricks[Math.floor(Math.random() * tricks.length)]
+const choose = tricks => tricks[Math.floor(Math.random() * tricks.length)];
